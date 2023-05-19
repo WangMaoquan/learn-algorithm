fd720b6487eb3ddaa6e0619e269e60bc9965a8af
wangmaoquan <1782976211@qq.com>
2023-05-19 16:21:53 +0800

    fix: 修改注解

--- src/Queue/slideWindow/index.ts
+++ src/Queue/slideWindow/index.ts
@@ -49,7 +49,7 @@ export const maxSlideWindow = (nums: number[], k: number) => {
 // 怎么维护?
 // 因为是 递减 所以 新的进入时 要保证之前的元素都要比 新的小, 即 尾出队列
 // 现在保证了 新的 进入队列 一定是递减了
-// 现在还有一种情况, 就是 当前队列 中的最大值 也就是 队列第一个元素 下标 不在 i - i+k 这个范围内 是不是应该 首出队列
+// 现在还有一种情况, 就是 当前队列 中的最大值 也就是 队列第一个元素 下标 不在 i - k + 1 ~ i 这个范围内 是不是应该 首出队列
 // 然后就是 啥时候开始 将 队列中的 第一个元素 push 进 result 数组里面 是不是遍历的 i 大于等于 k - 1 就可以了
 
 export const maxSlideWindowUseTwoEndQueue = (nums: number[], k: number) => {
