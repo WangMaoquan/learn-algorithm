e88ed2a9febc43d0eb11d3f7c5250a73a519de5e
decade <1782976211@qq.com>
2023-05-24 18:20:58 +0800

    feat: searchInsert

---
+++ src/Array/searchInsert/index.spec.ts
@@ -0,0 +1,9 @@
+import { describe, test, expect } from 'vitest';
+import { searchInsert } from '.';
+
+describe('搜索插入位置', () => {
+  test('search insert', () => {
+    expect(searchInsert([1, 2, 3, 4], 5)).toBe(4);
+    expect(searchInsert([1, 2, 3, 4], 4)).toBe(3);
+  });
+});
