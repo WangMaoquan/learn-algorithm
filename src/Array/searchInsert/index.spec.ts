561399353bf8f57755bf7774b4b01bc7dc4ce50f
decade <1782976211@qq.com>
2023-05-25 20:16:35 +0800

    fix: 增加测例

--- src/Array/searchInsert/index.spec.ts
+++ src/Array/searchInsert/index.spec.ts
@@ -5,5 +5,6 @@ describe('搜索插入位置', () => {
   test('search insert', () => {
     expect(searchInsert([1, 2, 3, 4], 5)).toBe(4);
     expect(searchInsert([1, 2, 3, 4], 4)).toBe(3);
+    expect(searchInsert([1, 3, 5, 6], 7)).toBe(4);
   });
 });
