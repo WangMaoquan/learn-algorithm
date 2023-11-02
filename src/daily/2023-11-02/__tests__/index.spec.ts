64887fd136490a0bdd5b8948d33afcb793ee929f
wangmaoquan <1782976211@qq.com>
2023-11-02 10:44:24 +0800

    feat: removeDuplicates

--- src/daily/2023-11-02/__tests__/index.spec.ts
+++ src/daily/2023-11-02/__tests__/index.spec.ts
@@ -1,6 +1,7 @@
 import { describe, it, expect } from 'vitest';
 import { removeElement } from '../first';
 import { removeDuplicates } from '../second';
+import { removeDuplicates as removeDuplicatesTrird } from '../third';
 
 describe('2023-11-02', () => {
   it('first', () => {
@@ -10,6 +11,11 @@ describe('2023-11-02', () => {
 
   it('second', () => {
     const r = removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);
+    expect(r).toBe(5);
+  });
+
+  it('third', () => {
+    const r = removeDuplicatesTrird([1, 1, 1, 2, 2, 3]);
     expect(r).toBe(5);
   });
 });
