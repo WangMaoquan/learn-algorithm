b4430f79e27b8697ead1cf804ec9e16f6e1c34b7
wangmaoquan <1782976211@qq.com>
2023-11-05 22:41:59 +0800

    feat: intToRoman

--- src/daily/2023-11-05/__tests__/index.spec.ts
+++ src/daily/2023-11-05/__tests__/index.spec.ts
@@ -2,6 +2,8 @@ import { describe, it, expect } from 'vitest';
 import { lengthOfLastWord } from '../first';
 import { longestCommonPrefix } from '../second';
 import { strStr } from '../third';
+import { intToRoman } from '../fourth';
+import { isPalindrome } from '../fifth';
 
 describe('2023-11-05', () => {
   it('first', () => {
@@ -26,5 +28,16 @@ describe('2023-11-05', () => {
 
     const r1 = strStr('leetcode', 'leeto');
     expect(r1).toBe(-1);
+  });
+
+  it('fourth', () => {
+    const r = intToRoman(3);
+    expect(r).toBe('III');
+
+    const r1 = intToRoman(58);
+    expect(r1).toBe('LVIII');
+
+    const r2 = intToRoman(1994);
+    expect(r2).toBe('MCMXCIV');
   });
 });
