40bb6d6a67d38bd61a03a67f2e4fe4768d0cce46
wangmaoquan <1782976211@qq.com>
2023-11-06 20:34:40 +0800

    feat: isIsomorphic

--- src/daily/2023-11-06/__tests__/index.spec.ts
+++ src/daily/2023-11-06/__tests__/index.spec.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from 'vitest';
 import { canConstruct } from '../first';
+import { isIsomorphic } from '../second';
 
 describe('2023-11-06', () => {
   it('first', () => {
@@ -8,6 +9,15 @@ describe('2023-11-06', () => {
     const r1 = canConstruct('aa', 'ab');
     expect(r1).toBe(false);
     const r2 = canConstruct('aa', 'aab');
+    expect(r2).toBe(true);
+  });
+
+  it('second', () => {
+    const r = isIsomorphic('egg', 'add');
+    expect(r).toBe(true);
+    const r1 = isIsomorphic('foo', 'bar');
+    expect(r1).toBe(true);
+    const r2 = isIsomorphic('paper', 'title');
     expect(r2).toBe(true);
   });
 });
