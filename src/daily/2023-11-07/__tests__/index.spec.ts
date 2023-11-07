ab51d83cc1208cd01e177a63912b803b1d57ff4c
wangmaoquan <1782976211@qq.com>
2023-11-07 17:46:22 +0800

    feat: isValid

--- src/daily/2023-11-07/__tests__/index.spec.ts
+++ src/daily/2023-11-07/__tests__/index.spec.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it } from 'vitest';
 import { isHappy } from '../first';
 import { containsNearbyDuplicate } from '../second';
+import { isValid } from '../third';
 
 describe('2023-11-07', () => {
   it('first', () => {
@@ -19,6 +20,17 @@ describe('2023-11-07', () => {
     expect(r1).toBe(true);
 
     const r2 = containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2);
+    expect(r2).toBe(false);
+  });
+
+  it('second', () => {
+    const r = isValid('()');
+    expect(r).toBe(true);
+
+    const r1 = isValid('()[]{}');
+    expect(r1).toBe(true);
+
+    const r2 = isValid('(]');
     expect(r2).toBe(false);
   });
 });
