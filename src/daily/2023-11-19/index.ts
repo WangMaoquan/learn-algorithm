2b362f329efe817a0f8938f8188fcf77177b0939
wangmaoquan <1782976211@qq.com>
2023-11-19 20:59:14 +0800

    refactoy: minSubArrayLen

--- src/daily/2023-11-19/index.ts
+++ src/daily/2023-11-19/index.ts
@@ -37,7 +37,7 @@
  *  所以我们首先想到的是 怎么去维护
  */
 
-export function minSubArrayLen(target: number, nums: number[]): number {
+export function minSubArrayLen1(target: number, nums: number[]): number {
   const r: number[] = [];
   let len = Infinity;
   let currSum = 0;
@@ -50,6 +50,25 @@ export function minSubArrayLen(target: number, nums: number[]): number {
       len = len < r.length ? len : r.length;
       currSum -= r.shift()!;
     }
+  }
+  return len === Infinity ? 0 : len;
+}
+
+// 完全没必要 使用 r 这样的数组, 我们只需要长度 长度 怎么来? 下标减
+
+export function minSubArrayLen(target: number, nums: number[]): number {
+  let start = 0; // 其实就是 r[0]
+  let end = 0; // 其实就是 r[r.length - 1]
+  let len = Infinity;
+  let currSum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    currSum += nums[i];
+    while (currSum >= target) {
+      const subLen = end - start + 1;
+      len = len < subLen ? len : subLen;
+      currSum -= nums[start++]!;
+    }
+    end++;
   }
   return len === Infinity ? 0 : len;
 }
