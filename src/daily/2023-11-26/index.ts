39f06db7fa5c1219d9f3aa6fd2959805f82640f2
wangmaoquan <1782976211@qq.com>
2023-11-26 21:31:54 +0800

    fix: 修复问题

--- src/daily/2023-11-26/index.ts
+++ src/daily/2023-11-26/index.ts
@@ -32,20 +32,24 @@ intervals[i].length == 2
  */
 
 export function merge(intervals: number[][]): number[][] {
+  if (intervals.length === 1) {
+    return intervals;
+  }
   const r: number[][] = [];
 
   intervals.sort((a, b) => a[0] - b[0]);
 
   for (let i = 1; i < intervals.length; i++) {
     if (intervals[i][0] <= intervals[i - 1][1]) {
-      r.push([
+      intervals[i] = [
         intervals[i - 1][0],
         Math.max(intervals[i - 1][1], intervals[i][1]),
-      ]);
+      ];
     } else {
-      r.push(intervals[i]);
+      r.push(intervals[i - 1]);
     }
   }
+  r.push(intervals.at(-1)!);
 
   return r;
 }
