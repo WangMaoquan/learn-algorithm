5d691abfbd54cf7e6792c4ba9370f87d3536aadd
wangmaoquan <1782976211@qq.com>
2023-11-27 13:45:30 +0800

    test: longestConsecutive

--- src/daily/2023-11-27/__tests__/index.spec.ts
+++ src/daily/2023-11-27/__tests__/index.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { groupAnagrams } from '..';
+import { groupAnagrams, longestConsecutive } from '..';
 
 describe('2023-11-27', () => {
   it('字母异位词分组', () => {
@@ -12,5 +12,13 @@ describe('2023-11-27', () => {
 
     const r2 = groupAnagrams(['a']);
     expect(r2).toEqual([['a']]);
+  });
+
+  it('最长连续序列', () => {
+    const r = longestConsecutive([100, 4, 200, 1, 3, 2]);
+    expect(r).toEqual(4);
+
+    const r1 = longestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]);
+    expect(r1).toEqual(9);
   });
 });
--- src/daily/2023-11-27/index.ts
+++ src/daily/2023-11-27/index.ts
@@ -50,3 +50,55 @@ export function groupAnagrams(strs: string[]): string[][] {
 
   return r;
 }
+
+/*
+
+给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+
+请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+
+
+
+示例 1：
+
+输入：nums = [100,4,200,1,3,2]
+输出：4
+解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
+示例 2：
+
+输入：nums = [0,3,7,2,5,8,4,6,0,1]
+输出：9
+
+
+提示：
+
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+
+*/
+
+/*
+
+是否能连续? 即 存在 curr + 1. 
+
+怎么判断存在? 遍历寻找 or map 存在
+
+*/
+
+export function longestConsecutive(nums: number[]): number {
+  const set = new Set(nums);
+  let r = 0;
+  for (let i = 0; i < nums.length; i++) {
+    let curr = nums[i];
+    // 不判断 会超时, 因为长度为 1 其实我们没必要去 while
+    if (!set.has(curr - 1)) {
+      let cr = 1;
+      while (set.has(curr + 1)) {
+        curr++;
+        cr++;
+      }
+      r = Math.max(cr, r);
+    }
+  }
+  return r;
+}
