82cd222b861dd21d02545172db14f3fdf23361e2
wangmaoquan <1782976211@qq.com>
2023-11-29 13:08:34 +0800

    test: plusOne

--- src/daily/2023-11-29/__tests__/index.spec.ts
+++ src/daily/2023-11-29/__tests__/index.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { isPalindrome } from '..';
+import { isPalindrome, plusOne } from '..';
 
 describe('2023-11-29', () => {
   it('回文数', () => {
@@ -11,5 +11,19 @@ describe('2023-11-29', () => {
 
     const r2 = isPalindrome(10);
     expect(r2).toBe(false);
+  });
+
+  it('加一', () => {
+    const r = plusOne([1, 2, 3]);
+    expect(r).toEqual([1, 2, 4]);
+
+    const r1 = plusOne([4, 3, 2, 1]);
+    expect(r1).toEqual([4, 3, 2, 2]);
+
+    const r2 = plusOne([0]);
+    expect(r2).toEqual([1]);
+
+    const r3 = plusOne([9]);
+    expect(r3).toEqual([1, 0]);
   });
 });
